9291a60a2aa103d21a15f1eb36af360833f475af
ASUS <kati.kheirabadi@gmail.com>
2023-06-15 22:55:29 +0430

    New way than make time shorter (1000000000 in 55s)

--- Application/Program.cs
+++ Application/Program.cs
@@ -2,19 +2,23 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 
-//var list = new ConcurrentBag<GuIdHolder>();
-var n = 600000000;
+var n = 1000000000;
 var list = new GuIdHolder[n];
 var watch = new Stopwatch();
 var maxthread = 0;
 ThreadPool.GetMaxThreads(out maxthread, out int a);
 var options = new ParallelOptions()
 {
-    MaxDegreeOfParallelism = maxthread/100
+    MaxDegreeOfParallelism = maxthread / 100
 
 };
 watch.Start();
-Parallel.For(0, n,options, i => list[i] = new GuIdHolder(Guid.NewGuid()));
+var firstTask =Task.Factory.StartNew(() => Parallel.For(0, n/3, options, i => list[i] = new GuIdHolder(Guid.NewGuid())));
+var secendTask =Task.Factory.StartNew(() => Parallel.For((n/3)+1, (n/3)*2, options, i => list[i] = new GuIdHolder(Guid.NewGuid())));
+var thirdTask =Task.Factory.StartNew(() => Parallel.For(((n / 3) * 2)+1, n, options, i => list[i] = new GuIdHolder(Guid.NewGuid())));
+Task.WaitAll(firstTask,secendTask,thirdTask);
+//old way
+//Parallel.For(0, n, options, i => list[i] = new GuIdHolder(Guid.NewGuid()));
 watch.Stop();
 var time = watch.Elapsed;
 Console.WriteLine("-----------------------------------------------------");
