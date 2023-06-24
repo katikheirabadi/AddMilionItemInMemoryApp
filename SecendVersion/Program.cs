10b7b555e875c4843ed546d91a5e0461fb7e3ad4
ASUS <kati.kheirabadi@gmail.com>
2023-06-25 00:56:11 +0430

    try to write new version of this projet

---
+++ SecendVersion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
+
+var structure = new BufferBlock<GuIdHolder>();
+var size = 500000000;
+var watch = new Stopwatch();
+watch.Start();
+Parallel.For(0, size, i => structure.Post(new GuIdHolder(new Guid())));
+watch.Stop();
+var time = watch.Elapsed;
+Console.WriteLine($"Time :{time.Hours}:{time.Minutes}:{time.Seconds}");
+
+public struct GuIdHolder
+{
+    private readonly Guid id;
+    public GuIdHolder(Guid id)
+    {
+
+        this.id = id;
+
+    }
+}
